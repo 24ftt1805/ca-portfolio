68542567131846aea4a052f1ffeee47c3f85f60e
24ftt1805 <24FTT1805@student.pb.edu.bn>
2025-08-11 12:46:35 +0800

    Add files via upload

---
+++ p2/DisplayPattern2.java
@@ -0,0 +1,17 @@
+public class DisplayPattern2 {
+    public static void main(String[] args) {
+        
+        String quote = "";
+
+        System.out.println("   .=\"\"=.\r" +quote);
+        System.out.println("  /      \\\r" +quote);
+        System.out.println(" | O    O | \r" +quote);
+        System.out.println(" \\   /\\   /\r" +quote);
+        System.out.println(" /'-=\\/=-'\\\r" +quote);
+        System.out.println("/ /      \\ \\     CHIP CHIP!");      
+        System.out.println("\\/\\      /\\/\r" +quote);     
+        System.out.println("   '.  .'\r" +quote);
+        System.out.println("   /|\\/|\\");                                   
+
+    }
+}
